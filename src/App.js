4d444021c48dc87fd5782346e5903f7810dc21ac
Dequone Jackson <Jacksond5@lakeland.edu>
2024-05-11 12:03:00 -0500

    created adding app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0)
+  const [num, setNum] = useState(0)
+
+  const add = (n) => {
+    setCount(count+n)
+    return
+  }
+  function handleNumChange(n) {
+    setNum(n)
+    return
+  }
   return (
     <div className="App">
-      
+      <h1 className="text">
+        {count}
+      </h1>
+      <input value={num} onChange={(e) =>
+        handleNumChange(parseInt(e.target.value))
+      }></input>
+      <button onClick={() => add(num)}>add</button>
     </div>
   );
 }
